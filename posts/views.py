8aabddbd4e67596c5b3c2851adffdddff615f78a
Temirlan <kat91971kara@gmail.com>
2025-07-23 23:36:48 +0600

    hw 5

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponse, redirect
 import random
 from posts.models import Post
 from posts.forms import PostForm, PostModelForm
+from django.contrib.auth.decorators import login_required
 
 def home(request):
      if request.method == "GET":
@@ -9,16 +10,20 @@ def home(request):
      else:
           return HttpResponse("HELLO YOU'RE AT THE POLIS INDEX")
 
+
+@login_required(login_url="/login")
 def post_list_view(request):
      if request.method == "GET":
           posts = Post.objects.all()
           return render(request, "posts/post_list.html", context={'posts': posts})
 
+@login_required(login_url="/login")
 def post_detail_view(request, post_id):
      if request.method == "GET":
           post = Post.objects.filter(id=post_id).first()
           return render(request, "posts/post_detail.html", context={"post": post})
-     
+
+@login_required(login_url="/login")
 def post_create_view(request):
      if request.method == "GET":
           form = PostModelForm()
